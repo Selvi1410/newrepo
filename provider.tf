ed0fa8654f0263437ae91285d4887f5c3bd611ce
Selvi1410 <88480364+Selvi1410@users.noreply.github.com>
2022-11-15 15:37:03 +0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
-  region  = var.aws_region
-  
+  region = "ap-southeast-1"
 }
