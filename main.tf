7dcc33526a84f10649a7c64605b32225943b2e39
Selvi1410 <88480364+Selvi1410@users.noreply.github.com>
2022-11-16 11:14:39 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "myec2" {
   ami = var.ec2_ami_id
   instance_type = var.instance_type
+  region = var.aws_region
   tags = {
     "Owner" = "user"
     "Environment" = "dev"
