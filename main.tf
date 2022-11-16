9a8aa61d075b2263c5b555e3a1f914ccb8f66f32
Selvi1410 <88480364+Selvi1410@users.noreply.github.com>
2022-11-16 20:37:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,23 @@ resource "aws_instance" "myec2" {
   ami = var.ec2_ami_id
   instance_type = var.instance_type
   tags = {
-    "Owner" = "user"
-    "Environment" = "dev"
+    Owner       = "user"
+    Environment = "dev"
+    Name = " var.instance_name"
+    Project = "Data Ingestion - Batch 1"
+    SecurityZone = "Zone 3B"
+    Description = "Snaplogic VM"
+    MarketPlaceAMI ="N"
+    CostCentre ="DnA"
+    Environment = "Workload Production Platform"
+    Application = "Data Ingestion - Batch 1"
+    Program = "Data Ingestion - Batch 1"
+    Shutdown = "None"
+    Owner = "var.owner_name"
+    Portfolio = "Workday"
+    Backup = "Daily1600"
+    MonthlyPatching = "Linux_Prod"
+    TicketNumber = "SCTASK0428011"
   }
  
 }
